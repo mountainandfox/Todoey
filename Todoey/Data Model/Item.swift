3d9019e5a04470286225e31b89cf95bbb0672422
Syed Rahman <syed.rahman@gmail.com>
2018-12-06 07:38:46 -0500

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Syed Rahman on 2018-12-06.
+//  Copyright © 2018 Mountain & Fox Inc. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
